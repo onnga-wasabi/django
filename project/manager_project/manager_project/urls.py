a0aa21c2fb51facaa3d6f59239647102bd44da7b
onnga-wasabi <yuuto.onnga.23@gmail.com>
2018-01-25 23:38:38 +0900

    delete

--- project/manager_project/manager_project/urls.py
+++ project/manager_project/manager_project/urls.py
@@ -1,4 +1,4 @@
-"""manager URL Configuration
+"""manager_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/

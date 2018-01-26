6115a9aee3483f12411930a09f1c237ccbc431d5
onnga-wasabi <yuuto.onnga.23@gmail.com>
2018-01-26 16:43:05 +0900

    add comment

--- project/mysite/polls/models.py
+++ project/mysite/polls/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+'''
+modelsに変更を加えたら
+python manage.py makemigrarions　でマイグレーションを作成
+python manage.py migrate　でデータベースにその変更を適用
+'''
 
 
 class Question(models.Model):

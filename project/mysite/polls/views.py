657c9a62ad99af8a31115104d580b824fb731b73
onnga-wasabi <yuuto.onnga.23@gmail.com>
2018-01-30 15:46:23 +0900

    fix bug

--- project/mysite/polls/views.py
+++ project/mysite/polls/views.py
@@ -8,11 +8,11 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
-def detail(request, qustion_id):
+def detail(request, question_id):
     return HttpResponse("You're lookin at quesion %s." % question_id)
 
 
-def results(request, qustion_id):
+def results(request, question_id):
     response = "You're looking at the results of question %s." % question_id)
     return HttpResponse(response % question_id)
 
